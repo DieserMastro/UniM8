631ee45c30561caf81f479d74a783dc135a6c9ae
DieserMastro <s0588238@htw-berlin.de>
2025-06-19 00:23:04 +0200

    successful experimentation with PDFbox
    - Create new PDFs
    - Open existing PDFs
    - Add a new blank page to either
    - Save PDF in desired directory

---
+++ src/main/java/unim8/PDF_Manager.java
@@ -0,0 +1,55 @@
+package unim8;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font; 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.io.MemoryUsageSetting;
+
+public class PDF_Manager {
+
+	public static void main(String[] args) throws IOException {
+		/*MemoryUsageSetting memUsage = MemoryUsageSetting.setupMainMemoryOnly();
+		memUsage.setUseMainMemoryOnly(true);
+        memUsage.setAllSecurityToBeRemoved(true);*/
+		//So far, this creates an empty pdf in the predetermined path
+		File oldFile = new File("C:\\Users\\sinan\\Desktop\\temp\\TestFolder\\currentLP.pdf");
+		PDDocument document = Loader.loadPDF(oldFile, "");
+		document.setAllSecurityToBeRemoved(true);
+		PDPage page_1 = new PDPage();
+		
+		document.addPage(page_1);
+		
+		
+		/*PDFTextStripper stripper = new PDFTextStripper();
+		String docContent = stripper.getText(document);
+		System.out.println(docContent);*/
+		
+		
+		
+		
+		
+		
+		
+		
+		try {
+			document.save("C:\\Users\\sinan\\Desktop\\temp\\TestFolder\\Test2.pdf");
+			System.out.println("File saved Successfully");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//System.out.println("File saved Successfully");
+		///
+		 
+		document.close();
+	}
+}
