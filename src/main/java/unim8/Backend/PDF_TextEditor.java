7c58f9fd8ac54fdda3c8cc30723a5fe43c76bb63
DieserMastro <s0588238@htw-berlin.de>
2025-06-19 16:23:26 +0200

    Update PDF_TextEditor.java

--- src/main/java/unim8/Backend/PDF_TextEditor.java
+++ src/main/java/unim8/Backend/PDF_TextEditor.java
@@ -1,5 +1,6 @@
 package unim8.Backend;
 
+import java.io.Console;
 import java.io.File;
 import java.io.IOException;
 
@@ -14,9 +15,76 @@ import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.io.MemoryUsageSetting;
 
 public class PDF_TextEditor {
-	public PDF_TextEditor(PDDocument doc) {
+	
+	PDDocument doc;
+	PDFTextStripper stripper = new PDFTextStripper();
+	PDPage default_Page = doc.getPage(0);
+	
+	public String extractText() {
+		String docText = "If this text shows, something went wrong! :(";
+		try {
+			docText = stripper.getText(doc);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Text extraction failed :(");
+		}
 		
+		return docText;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public PDF_TextEditor(PDDocument doc) {
+		this.doc = doc;
+	}
+	public PDDocument getPDDoc() {
+		return this.doc;
+	}
+	public void setPDDoc(PDDocument doc) {
+		this.doc = doc;
+	}
+	public PDPage getPage() {
+		return default_Page;
+	}
+	public PDPage getPage(int index) {
+		return doc.getPage(index);
+	}
+	public void setPage(PDPage page) {
+		this.default_Page = page;
+	}
+	
+	
 	
 	
 }
