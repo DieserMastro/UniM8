47de844638fdd3be23a1587da249ae71f85717ac
DieserMastro <s0588238@htw-berlin.de>
2025-06-30 22:11:46 +0200

    Update File_Manager.java

--- src/main/java/unim8/Backend/File_Manager.java
+++ src/main/java/unim8/Backend/File_Manager.java
@@ -3,6 +3,7 @@ package unim8.Backend;
 import java.nio.file.*;
 import java.awt.Desktop;
 import java.awt.desktop.*;
+import java.io.IOException;
 
 public class File_Manager {
 
@@ -15,8 +16,25 @@ public class File_Manager {
 	public void printPath() {
 		System.out.println(this.defaultPath);
 	}
-	public boolean checkIfFileExists(Path path) {
+	public boolean checkIfPathExists(Path path) {
 		boolean EXISTS = Files.exists(path);		
 		return EXISTS;
+	}
+	public void makeNewDirectory(Path path) throws IOException {
+		
+		if(checkIfPathExists(path)) {
+			System.out.println("Directory exists already");
+			return;
+		}
+		Files.createDirectory(path);
+		System.out.println(path + " has been created.");
+	}
+	public void createNewFile(Path path) throws IOException {
+		if(checkIfPathExists(path)) {
+			System.out.println("File exists already");
+			return;
+		}
+		Files.createFile(path);
+		System.out.println(path + " has been created.");
 	}
 }
