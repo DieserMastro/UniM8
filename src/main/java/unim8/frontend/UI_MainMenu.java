bcc91c0595771627d163a6fbfd3d2b39397f649e
DieserMastro <s0588238@htw-berlin.de>
2025-07-05 13:55:23 +0200

    Update UI_MainMenu.java

--- src/main/java/unim8/frontend/UI_MainMenu.java
+++ src/main/java/unim8/frontend/UI_MainMenu.java
@@ -3,6 +3,9 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -12,6 +15,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTree;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -20,7 +24,7 @@ import org.icepdf.ri.common.ComponentKeyBinding;
 import org.icepdf.ri.common.SwingController;
 import org.icepdf.ri.common.SwingViewBuilder;
 
-public class UI_MainMenu {
+public class UI_MainMenu implements ActionListener{
 	
 	private JFrame frame;
 	private JMenuBar menuBar;
@@ -71,6 +75,8 @@ public class UI_MainMenu {
 		this.menuBar = new JMenuBar();
 		//Images are 16px
 		this.fileMenu = new JMenu("File");
+		this.fileMenu.setMnemonic(KeyEvent.VK_F);
+		
 		this.newJMenu = new JMenu("New...");
 		ImageIcon defaultFileIcon = new ImageIcon("images/defaultFile_Icon.png");
 		this.newJMenu.setIcon(defaultFileIcon);
@@ -88,6 +94,9 @@ public class UI_MainMenu {
 		this.saveJMenuItem = new JMenuItem("Save");
 		ImageIcon defaultSaveIcon = new ImageIcon("images/defaultSave_Icon.png");
 		this.saveJMenuItem.setIcon(defaultSaveIcon);
+		this.saveJMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+		this.saveJMenuItem.addActionListener(this);
+
 		
 		this.exitJMenuItem = new JMenuItem("Exit");
 		ImageIcon defaultExitIcon = new ImageIcon("images/defaultExit_Icon.png");
@@ -101,5 +110,14 @@ public class UI_MainMenu {
 		this.menuBar.add(this.fileMenu);
 		
 		this.frame.setJMenuBar(this.menuBar);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() instanceof JMenuItem) {
+			JMenuItem item = (JMenuItem) e.getSource();
+			String text = item.getText();
+			System.out.println(text);
+		}
+		
 	}
 }
